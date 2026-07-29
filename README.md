d6c25048bafaf6097ec34ce12439ec2f0a2b1fab
mupindude <mupindudevine@gmail.com>
2026-07-29 08:45:41 +0400

    I started working on the data structures. I need to optimize argument parsing

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ This project has been created as part of the 42 curriculum by dmupindu
 Posix threads: (https://www.geeksforgeeks.org/operating-systems/posix-threads-in-os/)
 https://medium.com/@techdhaba.training/posix-threads-pthreads-the-simplest-way-to-understand-real-multithreading-in-c-c2f591ab7a03
 https://www.youtube.com/watch?v=ldJ8WGZVXZk&t=176s
+https://www.youtube.com/watch?v=d9s_d28yJq0&list=PLfqABt5AS4FmuQf70psXrsMLEDQXNkLq2
 
 #Challenges
 Pthreads are not natively supported on Windows
--- codexion.h
+++ codexion.h
@@ -1,6 +1,39 @@
 #ifndef CODEXION_H
 #define CODEXION_H
 
+typedef struct s_args
+{
+	int	nb_coders;
+	int	time_to_burnout;
+	int	time_to_compile;
+	int	time_to_debug;
+	int	time_to_refactor;
+	int	nb_compiles;
+	int	dongle_cooldown;
+	int	scheduler;
+}	t_args;
+
+
+typedef struct s_data
+{
+	t_coder	*coders;
+	t_dongle	*dongles;
+	pthread_t	monitoring;
+
+} t_data;
+
+typedef struct s_coder
+{
+	int	id;
+
+} t_coder;
+
+typedef struct s_dongle
+{
+	int	id;
+	int	cool_down;
+} t_dongle;
+
 #include <unistd.h>
 #include <stdio.h>
 #include <pthread.h>
