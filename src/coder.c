81c2496b3a541da3d4b64947e7e4ed6ea8252239
mupindude <mupindudevine@gmail.com>
2026-09-26 07:48:01 +0400

    The compile cycle is respecting the scheduling now. Working on the monitor to avoid burnout

--- src/coder.c
+++ src/coder.c
@@ -6,7 +6,7 @@
 /*   By: dmupindu <dmupindu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/14 07:34:18 by dmupindu          #+#    #+#             */
-/*   Updated: 2026/09/23 08:32:53 by dmupindu         ###   ########.fr       */
+/*   Updated: 2026/09/26 07:44:16 by dmupindu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 static void perform_compile(t_coder *coder);
 static void perform_debug(t_coder *coder);
+static void perform_refactor(t_coder *coder);
 
 void *coder_routine(void *arg)
 {
@@ -23,30 +24,40 @@ void *coder_routine(void *arg)
 
     coder = (t_coder *)arg;
     data = coder->data;
-    now = 0;
 
-    if (submit_request(data, coder, now) != 0)
-        return (NULL);
+	while (coder->compile_count < data->args.nb_compiles
+        && data->stop == 0)
+		{
+			now = get_time_ms();
 
-    pthread_mutex_lock(&data->scheduler_mutex);
-    while (coder->request->granted == 0 && data->stop == 0)
-        pthread_cond_wait(&data->scheduler_cond,
-            &data->scheduler_mutex);
-    pthread_mutex_unlock(&data->scheduler_mutex);
+			if (submit_request(data, coder, now) != 0)
+			return (NULL);
 
-    if (data->stop != 0)
-        return (NULL);
+			pthread_mutex_lock(&data->scheduler_mutex);
+			while (coder->request->granted == 0 && data->stop == 0)
+        		pthread_cond_wait(&data->scheduler_cond,
+            		&data->scheduler_mutex);
+			pthread_mutex_unlock(&data->scheduler_mutex);
+			if (data->stop != 0)
+					break;
 
-    pthread_mutex_lock(&data->print_mutex);
-    printf("Coder %d received permission to compile\n", coder->id);
-    pthread_mutex_unlock(&data->print_mutex);
+			pthread_mutex_lock(&data->print_mutex);
+			printf("Coder %d received permission to compile\n", coder->id);
+    		pthread_mutex_unlock(&data->print_mutex);
 
-    perform_compile(coder);
-    release_dongles(coder->request);
-    free(coder->request);
-    coder->request = NULL;
+			perform_compile(coder);
+    		release_dongles(coder->request);
 
-	perform_debug(coder);
+			free(coder->request);
+    		coder->request = NULL;
+
+			perform_debug(coder);
+			perform_refactor(coder);
+		}
+	pthread_mutex_lock(&data->print_mutex);
+	printf("Coder %d completed %d compiles\n",
+    coder->id, coder->compile_count);
+	pthread_mutex_unlock(&data->print_mutex);
 
     return (NULL);
 }
@@ -78,6 +89,17 @@ static void perform_debug(t_coder *coder)
     pthread_mutex_unlock(&data->print_mutex);
 
     usleep(data->args.time_to_debug * 1000);
+}
+
+static void perform_refactor(t_coder *coder)
+{
+    t_data  *data;
+
+    data = coder->data;
+    pthread_mutex_lock(&data->print_mutex);
+    printf("Coder %d is refactoring\n", coder->id);
+    pthread_mutex_unlock(&data->print_mutex);
+    usleep(data->args.time_to_refactor * 1000);
 }
 
 /*static	void take_dongle(t_dongle  *dongle)
